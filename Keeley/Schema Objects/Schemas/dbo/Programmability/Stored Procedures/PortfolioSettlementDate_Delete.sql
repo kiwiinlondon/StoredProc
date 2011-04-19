1d6ea9eb90dccb6e64ab6f4e484d9f442a38c12e
Geoff Poore <g.poore@odey.com>
2011-04-19 09:26:59 +0000

    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/StoredProcedureGenerator@517 a99c24c5-d062-0543-ab10-d6d61a48e989

---
+++ Keeley/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PortfolioSettlementDate_Delete.sql
@@ -0,0 +1,33 @@
+USE Keeley
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF EXISTS (SELECT * FROM sysobjects WHERE id = OBJECT_ID(N'[PortfolioSettlementDate_Delete]')
+AND OBJECTPROPERTY(id, N'IsProcedure') = 1)
+
+DROP PROCEDURE DBO.[PortfolioSettlementDate_Delete]
+GO
+
+CREATE PROCEDURE DBO.[PortfolioSettlementDate_Delete]
+		@PortfolioSettlementDateId int,
+		@DataVersion RowVersion,
+		@UpdateUserID int
+AS
+	SET NOCOUNT ON
+
+	DECLARE @EndDt DateTime
+	Set @EndDt = GetDate()
+
+	INSERT INTO PortfolioSettlementDate_hst (
+			PortfolioSettlementDateId, PositionAccountID, ReferenceDate, NetPosition, NetCostInstrumentCurrency, NetCostBookCurrency, DeltaNetCostInstrumentCurrency, DeltaNetCostBookCurrency, TodayNetPostionChange, TodayDeltaNetCostChangeInstrumentCurrency, TodayDeltaNetCostChangeBookCurrency, TodayNetCostChangeInstrumentCurrency, TodayNetCostChangeBookCurrency, StartDt, UpdateUserID, DataVersion, EndDt, LastActionUserID)
+	SELECT	PortfolioSettlementDateId, PositionAccountID, ReferenceDate, NetPosition, NetCostInstrumentCurrency, NetCostBookCurrency, DeltaNetCostInstrumentCurrency, DeltaNetCostBookCurrency, TodayNetPostionChange, TodayDeltaNetCostChangeInstrumentCurrency, TodayDeltaNetCostChangeBookCurrency, TodayNetCostChangeInstrumentCurrency, TodayNetCostChangeBookCurrency, StartDt, UpdateUserID, DataVersion, @EndDt, @UpdateUserID
+	FROM	PortfolioSettlementDate
+	WHERE	PortfolioSettlementDateId = @PortfolioSettlementDateId
+
+	DELETE	PortfolioSettlementDate
+	WHERE	PortfolioSettlementDateId = @PortfolioSettlementDateId
+	AND		DataVersion = @DataVersion
+GO
